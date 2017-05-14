8c560268f78ed35b3ec0b5300b42f79fede4696e
Nikita Basargin <nbasargin@web.de>
2017-05-14 19:56:15 +0200

    [v0.0.1] static orthographic WebGL demo

--- src/app.ts
+++ src/app.ts
@@ -37,21 +37,30 @@ export let browserEntryPoint = function() {
 
 
     // draw scene!
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clearColor(0.0, 0.5, 0.2, 1.0);
     gl.enable(gl.DEPTH_TEST);
 
     gl.viewport(0, 0, glContext.viewportWidth, glContext.viewportHeight);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    glMatrix.mat4.perspective (pMatrix, 45.0, glContext.viewportWidth / glContext.viewportHeight, 0.1, 100.0);
-    glMatrix.mat4.identity(mvMatrix);
+
+    // uncomment to enable perspective
+    //glMatrix.mat4.perspective (pMatrix, 45.0, glContext.viewportWidth / glContext.viewportHeight, 0.1, 100.0);
+    // orthographic
+    glMatrix.mat4.identity(pMatrix);
+
     // triangle
-    glMatrix.mat4.translate(mvMatrix, mvMatrix, [-1.5, 0.0, -5.4]);
+    glMatrix.mat4.identity(mvMatrix);
+    glMatrix.mat4.translate(mvMatrix, mvMatrix, [-0.5, 0.5, 0.0]);
+    glMatrix.mat4.scale(mvMatrix, mvMatrix, [0.4, 0.4, 0.4]);
     gl.bindBuffer(gl.ARRAY_BUFFER, triangleBuffer.buffer);
     gl.vertexAttribPointer(glContext.vertexPositionAttribute, triangleBuffer.itemSize, gl.FLOAT, false, 0, 0);
     glContext.setMatrixUniforms(pMatrix, mvMatrix);
     gl.drawArrays(gl.TRIANGLES, 0, triangleBuffer.numItems);
+
     // quad
-    glMatrix.mat4.translate(mvMatrix, mvMatrix, [3.0, 0.0, 0.0]);
+    glMatrix.mat4.identity(mvMatrix);
+    glMatrix.mat4.translate(mvMatrix, mvMatrix, [0.5, -0.5, 0.0]);
+    glMatrix.mat4.scale(mvMatrix, mvMatrix, [0.4, 0.4, 0.4]);
     gl.bindBuffer(gl.ARRAY_BUFFER, quadBuffer.buffer);
     gl.vertexAttribPointer(glContext.vertexPositionAttribute, quadBuffer.itemSize, gl.FLOAT, false, 0, 0);
     glContext.setMatrixUniforms(pMatrix, mvMatrix);
