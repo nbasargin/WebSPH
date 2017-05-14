618e42cb7a364afec0e48f58536882145e2dbe53
Nikita Basargin <nbasargin@web.de>
2017-05-14 13:50:33 +0200

    added js library folder

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 import {ShaderLoader} from "./util/shader-loader";
 
+let glMatrix = require('../js-lib/gl-matrix-2.2.1.js');
+
 export let browserEntryPoint = function() {
     console.log("dummy vertex shader:\n", ShaderLoader.getDummyVertShader());
     console.log("dummy fragment shader:\n", ShaderLoader.getDummyFragShader());
+
+    console.log(glMatrix.vec3.fromValues(1,2,3));
 };
