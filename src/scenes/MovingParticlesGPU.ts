1e67c264472a91aade6eb95259eebeb608e5be3b
Nikita Basargin <nbasargin@web.de>
2017-06-06 23:42:06 +0200

    skeleton of GPU scene

---
+++ src/scenes/MovingParticlesGPU.ts
@@ -0,0 +1,19 @@
+import {Scene} from "./Scene";
+import {GLContext} from "../util/GLContext";
+
+export class MovingParticlesGPU extends Scene {
+
+    public constructor(glContext : GLContext) {
+        super(glContext);
+
+    }
+
+    public update(dt: number): void {
+
+    }
+
+    public render(): void {
+
+    }
+
+}
