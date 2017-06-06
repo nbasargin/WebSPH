bc85d04883a88757275c87e44a5eac2c81954d2c
Nikita Basargin <nbasargin@web.de>
2017-06-06 22:37:14 +0200

    cleanup: removed two outdated scenes

--- src/sph/Particle.ts
+++ src/sph/Particle.ts
@@ -2,25 +2,16 @@ export class Particle {
 
     public pos : Array<number>;
     public speed : Array<number>;
-    public speedNew : Array<number>;
-    public mass : number;
-    public density : number;
-    public densityNew : number;
-    public color : Array<number>;
     public acceleration : number;
+
+    public color : Array<number>;
 
     public constructor() {
         this.pos = [0, 0, 0];
         this.speed = [0, 0, 0];
-        this.speedNew = [0, 0, 0];
-        this.mass = 1;
-        this.density = 1;
-        this.densityNew = 1;
-        this.color = [0, 0, 0, 0];
         this.acceleration = 0;
+
+        this.color = [0, 0, 0, 0];
     }
-
-
-
 
 }
