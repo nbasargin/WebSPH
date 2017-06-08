8a79f9f150cbf78dbe41c4b4585cb43779b424e0
Nikita Basargin <nbasargin@web.de>
2017-06-08 12:28:27 +0200

    loading texture

--- src/scenes/DrawTexture.ts
+++ src/scenes/DrawTexture.ts
@@ -1,22 +1,53 @@
 import {Scene} from "./Scene";
 import {GLContext} from "../util/GLContext";
+import {GLTexture} from "../util/GLTexture";
+import {GLBuffer} from "../util/GLBuffer";
 
 export class DrawTexture extends Scene {
 
-    public image = require("../../textures/nehe.gif");
+    // texture
+    private image = require("../../textures/nehe.gif");
+    private neheTexture : GLTexture;
+
+    // buffers
+    private quadVertPosBuffer : GLBuffer;
+    private quadTexPosBuffer : GLBuffer;
 
     public constructor(glContext : GLContext) {
         super(glContext);
 
+        // shaders
+        // TODO
+
+        // buffers
+        let quadPos = [
+            -1.0, -1.0,  1.0,
+            1.0, -1.0,  1.0,
+            1.0,  1.0,  1.0,
+            -1.0,  1.0,  1.0
+        ];
+        this.quadVertPosBuffer = new GLBuffer(this.glContext.gl, new Float32Array(quadPos), 3);
+
+        let texPos = [
+            0.0, 0.0,
+            1.0, 0.0,
+            1.0, 1.0,
+            0.0, 1.0
+        ];
+        this.quadTexPosBuffer = new GLBuffer(this.glContext.gl, new Float32Array(texPos), 2);
+
+        // textures
+        this.neheTexture = new GLTexture(this.glContext.gl);
+        this.neheTexture.setImage(this.image);
 
     }
 
     public update(dt: number): void {
-
+        this.setOrthographicProjection(-1, 1);
     }
 
     public render(): void {
-
+        // TODO
     }
 
 }
--- src/util/GLTexture.ts
+++ src/util/GLTexture.ts
@@ -1,11 +1,28 @@
 export class GLTexture {
 
     public image : HTMLImageElement;
+    public texture : WebGLTexture;
     public gl : WebGLRenderingContext;
 
     public constructor(gl : WebGLRenderingContext) {
         this.gl = gl;
+        this.texture = gl.createTexture();
+    }
 
+    public setImage(path : string) {
+        this.image = new Image();
+        this.image.onload = this.imageLoaded;
+        this.image.src = path;
+    }
+
+    private imageLoaded() {
+        let gl = this.gl;
+        gl.bindTexture(gl.TEXTURE_2D, this.texture);
+        gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, this.image);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+        gl.bindTexture(gl.TEXTURE_2D, null);
     }
 
 }
