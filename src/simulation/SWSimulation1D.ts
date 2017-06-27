8edd4a8d00a7f9b2d143a81da031a8f7a48f68b3
Nikita Basargin <nbasargin@web.de>
2017-06-27 12:30:50 +0200

    improved time step calculation

--- src/simulation/SWSimulation1D.ts
+++ src/simulation/SWSimulation1D.ts
@@ -18,8 +18,11 @@ export class SWSimulation1D {
 
     public totalTime = 0;
 
+    private acceleration = 9.81;
+    private fluidVolume = 1;
+
     public constructor(numParticles : number, bounds : Bounds) {
-        this.env = new SWEnvironment1D(numParticles, bounds, 1, 9.81);
+        this.env = new SWEnvironment1D(numParticles, bounds, this.fluidVolume, this.acceleration);
         this.euler = new IntegratorEuler(this.env);
         this.heun = new IntegratorHeun(this.env);
     }
@@ -41,16 +44,17 @@ export class SWSimulation1D {
 
     /**
      * Calculate maximal time step:
-     * dt = smoothing length / max speed / 2
+     * dt = factor * smoothing length / (max particle speed + wave propagation speed)
      */
-    public getMaxTimeStep(smoothingLength : number = this.smoothingLength) {
+    public getMaxTimeStep(smoothingLength : number = this.smoothingLength) : number {
         let maxSpeed = 0;
         for (let i = 0; i < this.env.particles.length; i++) {
             maxSpeed = Math.max(maxSpeed, Math.abs(this.env.particles[i].speed[0]));
         }
+        let ci = Math.sqrt(this.acceleration * smoothingLength); // wave propagation speed
         if (maxSpeed > 0) {
-            let dt = smoothingLength / maxSpeed / 2;
-            return dt * 0.9;
+            let dt = smoothingLength / (maxSpeed + ci);
+            return dt * 0.20;
         }
         return 0.001; // no speed (could be initial condition) -> allow a small time step to get some real speed
     }
