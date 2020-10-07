1582b4c791b45d6a966817d82a913fb400d7c6e6
Nikita Basargin <nbasargin@web.de>
2020-10-07 23:46:05 +0200

    replace require with import

--- src/util/Defaults.ts
+++ src/util/Defaults.ts
@@ -1,10 +1,11 @@
+import { version } from '../../package.json';
 import { RendererOptions } from '../rendering/RendererOptions';
 import { SimulationOptions } from '../simulation/SimulationOptions';
 import { WetDamBreak } from './scenarios/WetDamBreak';
 
 export class Defaults {
 
-    public static readonly VERSION: string = require('../../package.json').version;
+    public static readonly VERSION: string = version;
 
     public static getDefaultSimulationOptions(): SimulationOptions {
         return new WetDamBreak().getSimulationOptions();
